010b92aabc0ec6e36f8ad9427aa561b60997f822
Uchkun Rakhimov <uchkunrakhimov@gmail.com>
2025-03-08 16:17:08 +0500

    fix: static file path

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -31,10 +31,10 @@ app.use("/api/forms", formRoutes);
 
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../ui/build")));
+  app.use(express.static(path.join(__dirname, "../../ui/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../ui/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../../ui/build", "index.html"));
   });
 }
 
